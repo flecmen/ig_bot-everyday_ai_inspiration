9f343615858c254cf89852fa94a6904fc811b3a6
David Flek <davidovkyflekovky@gmail.com>
2023-02-22 20:35:49 +0100

    Odstranění spamování aby nás ig zase nebloknul

--- app.py
+++ app.py
@@ -102,51 +102,12 @@ def post_daily_image():
     post_image(filename)
 
 
-def like_follow_comment():
-    try:
-        hashtag = random.choice(hashtags)
-        client = Client()
-        client.login(username=os.getenv("INSTAGRAM_USERNAME"),
-                     password=os.getenv("INSTAGRAM_PASSWORD"))
-        medias = client.hashtag_medias_recent(hashtag, 5)
-        print('liking posts ...')
-        for i, media in enumerate(medias):
-            client.media_like(media.id)
-            print(f"Liked post number {i+1} of hashtag {hashtag}")
-            if i % 2 == 0:
-                client.user_follow(media.user.pk)
-                print(f"Followd user {media.user.username}")
-                time.sleep(2)
-                client.media_comment(media.id, random.choice(comments))
-                print("Commented {comment} under post number {i+1}")
-            time.sleep(5)
-    except:
-        print('like_follow_commnet, failed, nevermind')
-
-
 if __name__ == '__main__':
     execution_times = {
         "post_image": 12,
         "like_follow_comment": 18,
     }
     while True:
-        current_time = time.localtime()
-        # ve 12:00
-        if current_time.tm_hour == 12:
-            post_daily_image()
-            next_run = 4 * 60 * 60
-        # v 18:00
-        elif current_time.tm_hour == 18:
-            like_follow_comment()
-            next_run = 18 * 60 * 60
-        else:
-            # Calculate time to the next scheduled run (noon or 6 PM)
-            next_noon = 12 - current_time.tm_hour
-            if next_noon <= 0:
-                next_noon = abs(next_noon)+12
-            next_6pm = 18 - current_time.tm_hour
-            if next_6pm <= 0:
-                next_6pm += abs(next_6pm)+18
-            next_run = min(next_noon, next_6pm) * 60 * 60
-        # Wait for the calculated time to the next run
+        post_daily_image()
+        next_run = 24 * 60 * 60
         time.sleep(next_run)
